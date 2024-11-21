c08df32445008a89874091dbba08be7fff4c8ad4
DoheumPark <pirbak@hanmail.net>
2024-11-21 17:02:17 +0900

    feed - feed 등록

---
+++ src/main/java/com/green/greengramver1/feed/FeedService.java
@@ -0,0 +1,58 @@
+package com.green.greengramver1.feed;
+
+import com.green.greengramver1.common.MyFileUtils;
+import com.green.greengramver1.feed.model.FeedPicDto;
+import com.green.greengramver1.feed.model.FeedPostReq;
+import com.green.greengramver1.feed.model.FeedPostRes;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FeedService {
+    private final FeedMapper mapper;
+    private final MyFileUtils myFileUtils;
+
+    public FeedPostRes postFeed(List<MultipartFile> pics, FeedPostReq p) {
+
+        FeedPostRes res = new FeedPostRes();
+        int result = mapper.insFeed(p);
+
+        //파일 저장
+        //middlePath: feed/${feedId}
+        long feedId = p.getFeedId();
+        String middlePath = String.format("feed/%d", feedId);
+
+        //폴더 만들기
+        myFileUtils.makeFolders(middlePath);
+
+        //파일 저장
+        FeedPicDto feedPicDto = new FeedPicDto();
+        feedPicDto.setFeedId(feedId);
+        //feedPicDto에 feedId값 넣어주세요.
+        res.setFeedId(feedId);
+        List<String> picList = new ArrayList<>(pics.size());
+        for(MultipartFile pic : pics) {
+            String savedPicName = myFileUtils.makeRandomFileName(pic);
+            String filePath = String.format("%s/%s", middlePath, savedPicName);
+            try {
+                myFileUtils.transferTo(pic, filePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            feedPicDto.setPic(savedPicName);
+            picList.add(savedPicName);
+
+            mapper.insFeedPic(feedPicDto); //만들어주세요.
+        }
+        res.setPics(picList);
+        return res;
+    }
+}
---
+++ src/main/java/com/green/greengramver1/feed/model/FeedPicDto.java
@@ -0,0 +1,11 @@
+package com.green.greengramver1.feed.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FeedPicDto {
+    private long feedId;
+    private String pic;
+}
